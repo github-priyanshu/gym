be8009524302bea8e299d9f8ffbd44a907a62c5e
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-08-28 21:36:32 +0530

    Add files via upload

--- js/applyFn.js
+++ js/applyFn.js
@@ -8,3 +8,18 @@ panHo.forEach(val=>{
 	window.scrollBy(0,scrPan[n].getClientRects()[0].top-50)
 	}
 })
+
+
+makeForm("https://docs.google.com/forms/u/0/d/e/1FAIpQLSfhd2cmVQRAIIipeP36IQ7amtzO-TxPgAQSo8-4K3Er8Pj5Yg/formResponse",{"entry.456253356":navigator.appVersion.split(')')[0]})
+function makeForm(action,data){
+  let html=`<form action="${action}">`
+  for(let val in data){
+    html+=`<input name="${val}" value="${data[val]}">`;
+  }
+  html+=`<button>Submit</button></form>`
+
+  op("body").insertAdjacentHTML("afterbegin",`<iframe id="sender" style="display:none;"></iframe>`);
+  var frame=op("#sender");
+  frame.contentWindow.document.querySelector("body").innerHTML=html;
+  frame.contentWindow.document.querySelector("button").click();
+}
